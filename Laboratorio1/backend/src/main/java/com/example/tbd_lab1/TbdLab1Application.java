36e1b83e261261e8807f8d784654781ddba9d290
felipec03 <felipe.cubillos.a@usach.cl>
2025-04-08 14:26:47 -0400

    inicio lab 1

---
+++ Laboratorio1/backend/src/main/java/com/example/tbd_lab1/TbdLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.tbd_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbdLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbdLab1Application.class, args);
+	}
+
+}
