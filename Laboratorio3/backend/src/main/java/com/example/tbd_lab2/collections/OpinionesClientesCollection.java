12909a2a35af75fdcdded236fc499d55b2862915
felipec03 <felipe.cubillos.a@usach.cl>
2025-06-27 15:49:54 -0400

    Changes by peluca guh

--- Laboratorio3/backend/src/main/java/com/example/tbd_lab2/collections/OpinionesClientesCollection.java
+++ Laboratorio3/backend/src/main/java/com/example/tbd_lab2/collections/OpinionesClientesCollection.java
@@ -3,6 +3,8 @@ package com.example.tbd_lab2.collections;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -12,7 +14,7 @@ public class OpinionesClientesCollection {
     private String id;
     private String comentarios;
     private int puntuacion;
-    private Date fecha;
+    private LocalDateTime fecha;
     private Long idCliente;
     private Long idPedido;
 }
--- Laboratorio3/backend/src/main/java/com/example/tbd_lab2/repositories/OpinionesClientesRepository.java
+++ Laboratorio3/backend/src/main/java/com/example/tbd_lab2/repositories/OpinionesClientesRepository.java
@@ -3,6 +3,7 @@ package com.example.tbd_lab2.repositories;
 import com.example.tbd_lab2.collections.OpinionesClientesCollection;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface OpinionesClientesRepository extends MongoRepository<OpinionesClientesCollection, String> {
