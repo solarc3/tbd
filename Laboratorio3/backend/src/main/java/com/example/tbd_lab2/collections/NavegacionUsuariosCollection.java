d30ff754f07097b8be5dd416f33c394dfe1d6d8f
felipec03 <felipe.cubillos.a@usach.cl>
2025-06-27 15:48:41 -0400

    Changes by dodo huh

--- Laboratorio3/backend/src/main/java/com/example/tbd_lab2/collections/NavegacionUsuariosCollection.java
+++ Laboratorio3/backend/src/main/java/com/example/tbd_lab2/collections/NavegacionUsuariosCollection.java
@@ -1,6 +1,9 @@
 package com.example.tbd_lab2.collections;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,9 +19,16 @@ public class NavegacionUsuariosCollection {
     private List<EventoNavegacion> eventos;
 
     @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class EventoNavegacion {
-        private String tipo; // "busqueda", "click", "filtro"
+        private String tipo; // "busqueda", "click", "filtro", "compra"
         private String valor;
         private Date timestamp;
+    }
+
+    public void addLog(EventoNavegacion evento) {
+        eventos.add(evento);
     }
 }
